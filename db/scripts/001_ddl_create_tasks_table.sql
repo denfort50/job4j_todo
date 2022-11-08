c15c59e5c728bfd6e123df6edda041c2dc4ce459
denfort50 <denfort50@yandex.ru>
2022-11-08 18:48:01 +0300

    Добавлены скрипты SQL

---
+++ db/scripts/001_ddl_create_tasks_table.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS tasks (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR NOT NULL,
+    description VARCHAR NOT NULL,
+    created TIMESTAMP,
+    done BOOLEAN
+);
+
+COMMENT ON TABLE tasks IS 'Задачи';
+COMMENT ON COLUMN tasks.id IS 'Идентификатор задачи';
+COMMENT ON COLUMN tasks.title IS 'Название задачи';
+COMMENT ON COLUMN tasks.description IS 'Подробное описание задачи';
+COMMENT ON COLUMN tasks.created IS 'Дата создания задачи';
+COMMENT ON COLUMN tasks.done IS 'Флаг выполнения задачи';
---
+++ db/scripts/002_dml_insert_tasks.sql
@@ -0,0 +1,3 @@
+INSERT INTO tasks (title, description, created, done) VALUES ('Доработка задач' , 'Исправить задачи Job4j, отклоненные при проверке', '2022-11-06 16:00:00', true);
+INSERT INTO tasks (title, description, created, done) VALUES ('Ознакомление с требованиями' , 'Ознакомиться с требованиями к Web-проектам', '2022-11-06 17:00:00', true);
+INSERT INTO tasks (title, description, created, done) VALUES ('Реализация проекта' , 'Реализовать Web-проект ToDo', '2022-11-06 18:00:00', false);
