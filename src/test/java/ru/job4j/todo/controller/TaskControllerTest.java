358f9a3f85df68a965ab90c508dfe292f25888c7
denfort50 <denfort50@yandex.ru>
2022-11-08 18:59:14 +0300

    Добавлены тесты на методы контроллера

---
+++ src/test/java/ru/job4j/todo/controller/TaskControllerTest.java
@@ -0,0 +1,110 @@
+package ru.job4j.todo.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.ui.Model;
+import ru.job4j.todo.model.Task;
+import ru.job4j.todo.service.SimpleTaskService;
+import ru.job4j.todo.service.TaskService;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class TaskControllerTest {
+
+    @Test
+    void whenGetAllTasksThenSuccess() {
+        Task task1 = new Task(1, "Задача 1", "Описание 1", LocalDateTime.now());
+        Task task2 = new Task(2, "Задача 2", "Описание 2", LocalDateTime.now());
+        List<Task> tasks = List.of(task1, task2);
+        Model model = mock(Model.class);
+        TaskService taskService = mock(SimpleTaskService.class);
+        when(taskService.findAll()).thenReturn(tasks);
+        TaskController taskController = new TaskController(taskService);
+        String page = taskController.getAllTasks(model);
+        verify(model).addAttribute("allTasks", tasks);
+        assertThat(page).isEqualTo("allTasks");
+    }
+
+    @Test
+    void whenGetNewTasksThenSuccess() {
+        Task task1 = new Task(1, "Задача 1", "Описание 1", LocalDateTime.now());
+        Task task2 = new Task(2, "Задача 2", "Описание 2", LocalDateTime.now());
+        Task task3 = new Task(3, "Задача 3", "Описание 3", LocalDateTime.now());
+        task1.setDone(true);
+        List<Task> newTasks = List.of(task2, task3);
+        Model model = mock(Model.class);
+        TaskService taskService = mock(SimpleTaskService.class);
+        when(taskService.findNew()).thenReturn(newTasks);
+        TaskController taskController = new TaskController(taskService);
+        String page = taskController.getNewTasks(model);
+        verify(model).addAttribute("newTasks", newTasks);
+        assertThat(page).isEqualTo("newTasks");
+    }
+
+    @Test
+    void whenGetCompletedTasksThenSuccess() {
+        Task task1 = new Task(1, "Задача 1", "Описание 1", LocalDateTime.now());
+        Task task2 = new Task(2, "Задача 2", "Описание 2", LocalDateTime.now());
+        Task task3 = new Task(3, "Задача 3", "Описание 3", LocalDateTime.now());
+        task1.setDone(true);
+        task2.setDone(true);
+        task3.setDone(true);
+        List<Task> completedTasks = List.of(task1, task2, task3);
+        Model model = mock(Model.class);
+        TaskService taskService = mock(SimpleTaskService.class);
+        when(taskService.findCompleted()).thenReturn(completedTasks);
+        TaskController taskController = new TaskController(taskService);
+        String page = taskController.getCompletedTasks(model);
+        verify(model).addAttribute("completedTasks", completedTasks);
+        assertThat(page).isEqualTo("completedTasks");
+    }
+
+    @Test
+    void whenAddTaskThenSuccess() {
+        Task task1 = new Task(1, "Задача 1", "Описание 1", LocalDateTime.now());
+        Model model = mock(Model.class);
+        TaskService taskService = mock(SimpleTaskService.class);
+        TaskController taskController = new TaskController(taskService);
+        taskService.add(task1);
+        String page = taskController.addTask(model);
+        verify(taskService).add(task1);
+        assertThat(page).isEqualTo("addTask");
+    }
+
+    @Test
+    void whenCreateTaskThenSuccess() {
+        Task task1 = new Task(1, "Задача 1", "Описание 1", LocalDateTime.now());
+        TaskService taskService = mock(SimpleTaskService.class);
+        TaskController taskController = new TaskController(taskService);
+        String page = taskController.createTask(task1);
+        verify(taskService).add(task1);
+        assertThat(page).isEqualTo("redirect:/formAllTasks");
+    }
+
+//    @Test
+//    void whenGetTaskDescriptionThenSuccess() {
+//    }
+//
+//    @Test
+//    void whenCompleteTaskThenSuccess() {
+//    }
+//
+//    @Test
+//    void whenModifyTaskThenSuccess() {
+//    }
+//
+//    @Test
+//    void whenUpdateTaskThenSuccess() {
+//    }
+//
+//    @Test
+//    void whenDeleteTaskThenSuccess() {
+//    }
+//
+//    @Test
+//    void whenDeleteAllTasksThenSuccess() {
+//    }
+}
