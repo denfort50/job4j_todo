e944efdc2ab03a8a8f0234cda22fab6c17c87765
denfort50 <denfort50@yandex.ru>
2022-11-27 11:55:11 +0300

    Добавлен CascadeType.ALL

--- src/main/java/ru/job4j/todo/model/Task.java
+++ src/main/java/ru/job4j/todo/model/Task.java
@@ -39,7 +39,7 @@ public class Task {
 
     /** Принадлежность задачи к пользователю */
 
-    @ManyToOne
+    @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "user_id")
     private User user;
 
